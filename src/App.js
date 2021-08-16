ef711115d8fef81174eda2c204e5bce52686d2a4
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-08-16 18:09:18 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 /** @format */
 
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/header/Header";
 import { ContainerStyled } from "./components/container/ContainerStyled";
 import { HeaderStyled } from "./components/header/HeaderStyled";
 import Main from "./components/main/Main";
 import { MainStyled } from "./components/main/MainStyled";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getCurrentUser } from "./redux/operation/authOperation";
+import { useEffect } from "react";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.getCurrentUser();
-  }
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+  return (
+    <>
+      <HeaderStyled>
+        <ContainerStyled>
+          <Header />
+        </ContainerStyled>
+      </HeaderStyled>
+      <MainStyled>
+        <ContainerStyled>
+          <Main />
+        </ContainerStyled>
+      </MainStyled>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <HeaderStyled>
-          <ContainerStyled>
-            <Header />
-          </ContainerStyled>
-        </HeaderStyled>
-        <MainStyled>
-          <ContainerStyled>
-            <Main />
-          </ContainerStyled>
-        </MainStyled>
-      </>
-    );
-  }
-}
-
-export default connect(null, { getCurrentUser })(App);
+export default App;
